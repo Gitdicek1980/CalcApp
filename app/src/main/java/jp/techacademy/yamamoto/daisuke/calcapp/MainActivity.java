ff13afab17b51cb330c5b1ca7a51311f98b4fc1e
d_yamamoto <nice_day81@yahoo.co.jp>
2017-12-06 14:26:18 +0900

    初回UP

---
+++ app/src/main/java/jp/techacademy/yamamoto/daisuke/calcapp/MainActivity.java
@@ -0,0 +1,36 @@
+package jp.techacademy.yamamoto.daisuke.calcapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("VALUE1", 0);
+        intent.putExtra("VALUE2", 0);
+        startActivity(intent);
+    }
+}
